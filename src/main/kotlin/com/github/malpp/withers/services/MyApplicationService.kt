c9051c7d717197cf017c5a1ae570d2df146bd4a7
GitHub Action <action@github.com>
2020-07-19 21:39:28 +0000

    Template cleanup

--- src/main/kotlin/com/github/malpp/withers/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/malpp/withers/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.malpp.withers.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.malpp.withers.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/malpp/withers/services/MyProjectService.kt
+++ src/main/kotlin/com/github/malpp/withers/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.malpp.withers.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.malpp.withers.MyBundle
 
 class MyProjectService(project: Project) {
 
